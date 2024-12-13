79e7d2da7617edd47a88210330c071324fae54b4
Jelle de Jong <jelle.dejong@hu.nl>
2024-12-13 11:02:16 +0100

    fix publish script

--- publish-application.ps1
+++ publish-application.ps1
@@ -8,15 +8,13 @@ $gitBranchLocal = "main"
 $gitBranchRemote = "main"
 $gitRepoRemote = "https://github.com/jelle-de-jong-hu/GitHubPages.git"
 
-
-
 Remove-Item ".\$deployFolder\" -Force -Recurse -ErrorAction SilentlyContinue
 
-npm run build --prefix $npmRoot --output-path="$docs"
+npm run build --prefix $npmRoot
 
 Move-Item "$distFolder" $deployFolder -Force
 New-Item -Path "$deployFolder\.nojekyll" -Force
 
 git add "$deployFolder/*"
 git commit -m "$commitMessage"
-git push -f "$gitRepoRemote" "$($gitBranchLocal):$($main)"
+git push -f "$gitRepoRemote" "$($gitBranchLocal):$($gitBranchRemote)"
