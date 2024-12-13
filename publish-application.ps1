185d263c1474415a8977fe8067fe258f6a542451
Jelle de Jong <jelle.dejong@hu.nl>
2024-12-13 10:59:20 +0100

    Added vite config, tweaked publish script

--- publish-application.ps1
+++ publish-application.ps1
@@ -3,10 +3,20 @@ cls
 $deployFolder = "docs"
 $npmRoot = "githubpages"
 $distFolder = "$npmRoot\dist"
+$commitMessage = "publish"
+$gitBranchLocal = "main"
+$gitBranchRemote = "main"
+$gitRepoRemote = "https://github.com/jelle-de-jong-hu/GitHubPages.git"
 
-Remove-Item ".\$deployFolder\" -Force -Recurse
+
+
+Remove-Item ".\$deployFolder\" -Force -Recurse -ErrorAction SilentlyContinue
 
 npm run build --prefix $npmRoot --output-path="$docs"
 
 Move-Item "$distFolder" $deployFolder -Force
 New-Item -Path "$deployFolder\.nojekyll" -Force
+
+git add "$deployFolder/*"
+git commit -m "$commitMessage"
+git push -f "$gitRepoRemote" "$($gitBranchLocal):$($main)"
