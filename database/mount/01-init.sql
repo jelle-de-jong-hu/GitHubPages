adbc51ab4bedd21f4aae5594f74d371429386cb3
Lucas Brinkers <lucas.brinkers@hu.nl>
2026-01-08 11:20:26 +0100

    shell-init verwijderd, db-init gaat nu via SQL-mount in docker-compose

--- database/mount/01-init.sql
+++ database/mount/01-init.sql
@@ -1,6 +1,4 @@
-CREATE USER "hu-s4-lingo-dbadmin" WITH CREATEDB PASSWORD 'hu-s4-lingo-pwd';
-CREATE DATABASE "hu-s4-lingo" OWNER "hu-s4-lingo-dbadmin";
-
+-- Main DB/user is created via docker-compose
+-- Create test database and user for local/CI tests
 CREATE USER "hu-s4-lingo-dbadmin-test" WITH CREATEDB PASSWORD 'hu-s4-lingo-pwd';
 CREATE DATABASE "hu-s4-lingo-test" OWNER "hu-s4-lingo-dbadmin-test";
-
--- database/mount/02-init-hu-s4-lingo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-PGPASSWORD="hu-s4-lingo-pwd" psql \
-  --username="hu-s4-lingo-dbadmin" \
-  --dbname="hu-s4-lingo" \
-  -f "$SCRIPT_DIR/lingo_words.sql"
